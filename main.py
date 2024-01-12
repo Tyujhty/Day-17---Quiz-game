024b0876f2512f21c9ad41baca36624c04be3ff2
Tyujhty <fannybaquey@gmail.com>
2024-01-12 18:07:09 +0100

    feat: create method to test if there's still a question.

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ for question in question_data:
     new_q = Question(question['text'], question['answer'])
     question_bank.append(new_q)
 
-
 quiz = QuizzBrain(question_bank)
-quiz.next_question()
+
+while quiz.still_has_question():
+    quiz.next_question()
--- quiz_brain.py
+++ quiz_brain.py
@@ -3,7 +3,14 @@ class QuizzBrain:
         self.question_number = 0
         self.question_list = q_list
 
+    def still_has_question(self):
+        number_of_question = len(self.question_list)
+        if self.question_number < number_of_question:
+            return True
+
+
     def next_question(self):
         current_question = self.question_list[self.question_number]
         self.question_number += 1
         input(f"Q: {self.question_number}: {current_question.text} (True/False): ")
+
